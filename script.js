a2a0c8485e5d24a07b3b1b1b4e2133f52d893c29
José G. R. Launes <josegrlopes@gmail.com>
2024-07-30 10:56:54 +0200

    Added basic functions and main html elements

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-const testVar = 2;
+// Basic Functions
+const add = (a, b) => {
+    return a + b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+// Variables declaration
+const firstNumber = 0;
+
+const operator = '';
+
+const lastNumber = 0;
+
+// Operate function
+const operate = (firstNumber, operator, lastNumber) => {
+    switch (operator) {
+        case 'add':
+            return add(firstNumber, lastNumber);
+            break;
+        case 'subtract':
+            return subtract(firstNumber, lastNumber);
+            break;
+        case 'multiply':
+            return multiply(firstNumber, lastNumber);
+            break;
+        case 'divide':
+            return divide(firstNumber, lastNumber);
+            break;
+    }
+};
